d31e361d4d3d54500cdc94af5cb090c1f717b958
asm2212 <asmareadmasu0@gmail.com>
2024-11-19 21:47:38 +0300

    Fix side menu selection issue and improve state management

--- lib/widgets/side_menu_widget.dart
+++ lib/widgets/side_menu_widget.dart
@@ -1,3 +1,4 @@
+import 'package:fit_app_dashboard/const/constant.dart';
 import 'package:fit_app_dashboard/data/side_menu_data.dart';
 import 'package:flutter/material.dart';
 
@@ -9,14 +10,22 @@ class SideMenuWidget extends StatefulWidget {
 }
 
 class _SideMenuWidgetState extends State<SideMenuWidget> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     final data = SideMenuData();
     return Container(
+      padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 20),
       child: ListView.builder(
         itemCount: data.menu.length,
-        itemBuilder: (context,index) => buildMenuEntry(data,index),
-        ),
+        itemBuilder: (context, index) =>
+            buildMenuEntry(data, index, selectedIndex, (newIndex) {
+          setState(() {
+            selectedIndex = newIndex;
+          });
+        }),
+      ),
     );
   }
 }
@@ -24,24 +33,39 @@ class _SideMenuWidgetState extends State<SideMenuWidget> {
 Widget buildMenuEntry(
   SideMenuData data,
   int index,
-){
-  return Row(
-    children: [
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 13,vertical: 7),
-        child: Icon(
-          data.menu[index].icon,
-          color: Colors.grey,
-        ),
+  int selectedIndex,
+  ValueChanged<int> onItemSelected,
+) {
+  final isSelected = selectedIndex == index;
+  return Container(
+    margin: const EdgeInsets.symmetric(vertical: 5),
+    decoration: BoxDecoration(
+      borderRadius: const BorderRadius.all(
+        Radius.circular(6),
       ),
-      Text(
-        data.menu[index].title,
-        style: const TextStyle(
-          fontSize: 14,
-          color: Colors.grey,
-          fontWeight: FontWeight.normal,
-        ),
-      )
-    ],
+      color: isSelected ? selectionColor : Colors.transparent,
+    ),
+    child: InkWell(
+      onTap: () => onItemSelected(index),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 7),
+            child: Icon(
+              data.menu[index].icon,
+              color: isSelected ? Colors.black : Colors.grey,
+            ),
+          ),
+          Text(
+            data.menu[index].title,
+            style: TextStyle(
+              fontSize: 14,
+              color: isSelected ? Colors.black : Colors.grey,
+              fontWeight: isSelected ? FontWeight.w700 : FontWeight.normal,
+            ),
+          ),
+        ],
+      ),
+    ),
   );
 }
